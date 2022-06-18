ade4864e7d08d0c2290d6dd9521e7e2eb32db125
Nurullah Eroğlu <69904555+nurullaheroglu@users.noreply.github.com>
2022-06-18 07:12:12 +0300

    Update streamlit_app.py

--- ev_kira_fiyat_tahminleme/streamlit_app.py
+++ ev_kira_fiyat_tahminleme/streamlit_app.py
@@ -5,7 +5,9 @@ import pandas as pd
 from helper import *
 
 model = joblib.load("ev_kira_fiyat_tahminleme/final_model.pkl")
-df1 = pd.read_csv("ev_kira_fiyat_tahminleme/house_rent_prices_dataset.csv")
+df1 = pd.read_csv("ev_kira_fiyat_tahminleme/house_rent_prices_dataset.csv", index_col="Unnamed: 0").reset_index().drop("index", axis=1)
+
+#df1 = pd.read_csv("ev_kira_fiyat_tahminleme/house_rent_prices_dataset.csv")
 
 anadolu_list = set([ilce for i, ilce in enumerate(df1["ilce"]) if df1["yaka"][i] == "Anadolu"])
 avrupa_list = set([ilce for i, ilce in enumerate(df1["ilce"]) if df1["yaka"][i] == "Avrupa"])
